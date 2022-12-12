b64e0b33cd03efdbe26ba8e4eae08ac3e660c4d1
gorillaRouge <john.schoppmann@yahoo.com>
2022-12-12 11:42:52 -0700

    Added structure to my HTML, styles in my CSS.

---
+++ assets/javascript.js
@@ -0,0 +1,30 @@
+
+var city="";
+
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
